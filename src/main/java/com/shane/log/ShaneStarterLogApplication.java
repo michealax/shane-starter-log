4a935e78c96e9728796576589ef0475df28481e8
shane <michealax@github.com>
2024-06-04 02:17:25 +0800

    init

---
+++ src/main/java/com/shane/log/ShaneStarterLogApplication.java
@@ -0,0 +1,13 @@
+package com.shane.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaneStarterLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaneStarterLogApplication.class, args);
+    }
+
+}
